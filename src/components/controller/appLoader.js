bed084274f36b65dce9bc8a054c101302bd8fe7e
Littleflyy <sopho.kar@gmail.com>
2023-06-07 10:16:44 +0300

    feat: add apiKey

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '0d083c976b95401098338921d7406c48', // получите свой ключ https://newsapi.org/
         });
     }
 }
